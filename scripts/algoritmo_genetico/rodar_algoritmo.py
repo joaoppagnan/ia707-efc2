fe093ec27f5a5aeccd0e8d9e7b251de52fece6fa
joaoppagnan <joaopedroopagnan@gmail.com>
2023-07-18 21:03:24 -0300

    Acho q terminei

--- scripts/algoritmo_genetico/rodar_algoritmo.py
+++ scripts/algoritmo_genetico/rodar_algoritmo.py
@@ -2,10 +2,12 @@ import numpy as np
 from algoritmo_genetico import algoritmo_genetico
 
 
-N_POPULACAO = 1000
+N_POPULACAO = 100
 CRITERIO_DE_PARADA = 20
 Q_TORNEIO = 10
 P_MUTACAO = 0.5
+LIMITE_INFERIOR = -1.0
+LIMITE_SUPERIOR = 2
 PATH_MAT_DADOS = '../../dados/elshafei_QAP.mat'
 REALIZACOES = 10
 PATH_SALVAR_MELHORES_SOLUCOES = "../../melhores_solucoes/algoritmo-genetico/"
@@ -16,21 +18,19 @@ fitnesses = []
 custos = []
 
 for realizacao in range(0, REALIZACOES):
-    solucao, fitness, custo = algoritmo_genetico(n_populacao=N_POPULACAO, q_torneio=Q_TORNEIO,
+    solucao, fitness = algoritmo_genetico(n_populacao=N_POPULACAO, q_torneio=Q_TORNEIO,
                                                  criterio_de_parada=CRITERIO_DE_PARADA,
-                                                 p_mutacao=P_MUTACAO, path_mat_dados=PATH_MAT_DADOS,
-                                                 realizacao=realizacao, path_graficos=PATH_GRAFICOS)
+                                                 p_mutacao=P_MUTACAO, limite_inferior=LIMITE_INFERIOR,
+                                                 limite_superior=LIMITE_SUPERIOR, realizacao=realizacao,
+                                                 path_graficos=PATH_GRAFICOS)
     solucoes.append(solucao)
     fitnesses.append(fitness)
-    custos.append(custo)
 
 np.savetxt(fname=PATH_SALVAR_MELHORES_SOLUCOES+'solucoes.txt', X=np.array(solucoes).reshape((len(solucoes), 19)),
            delimiter=',', newline='\n', fmt='%i')
 np.savetxt(fname=PATH_SALVAR_MELHORES_SOLUCOES+'fitnesses.txt', X=np.array(fitnesses).reshape((len(fitnesses), 1)),
            delimiter=',', newline='\n', fmt='%.4f')
-np.savetxt(fname=PATH_SALVAR_MELHORES_SOLUCOES+'custos.txt', X=np.array(custos).reshape((len(custos), 1)),
+np.savetxt(fname=PATH_SALVAR_MELHORES_SOLUCOES+'fitnesses-valor-medio.txt', X=np.array([np.mean(fitnesses)]),
            delimiter=',', newline='\n', fmt='%i')
-np.savetxt(fname=PATH_SALVAR_MELHORES_SOLUCOES+'custos-valor-medio.txt', X=np.array([np.mean(custos)]),
-           delimiter=',', newline='\n', fmt='%i')
-np.savetxt(fname=PATH_SALVAR_MELHORES_SOLUCOES+'custos-desvio-padrao.txt', X=np.array([np.std(custos)]),
+np.savetxt(fname=PATH_SALVAR_MELHORES_SOLUCOES+'fitnesses-desvio-padrao.txt', X=np.array([np.std(fitnesses)]),
            delimiter=',', newline='\n')
