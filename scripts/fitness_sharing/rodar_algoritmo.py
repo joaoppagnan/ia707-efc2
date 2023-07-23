9f15a53ac11d79a6113bbf1b3f946bf5383f3a56
joaoppagnan <joaopedroopagnan@gmail.com>
2023-07-23 17:24:48 -0300

    Mais ajustes

--- scripts/fitness_sharing/rodar_algoritmo.py
+++ scripts/fitness_sharing/rodar_algoritmo.py
@@ -16,16 +16,19 @@ PATH_GRAFICOS = "../../graficos/fitness-sharing/"
 
 solucoes = []
 fitnesses = []
+fitnesses_sh = []
 custos = []
 
 for realizacao in range(0, REALIZACOES):
-    solucao, fitness = algoritmo_genetico(n_populacao=N_POPULACAO, q_torneio=Q_TORNEIO,
-                                          criterio_de_parada=CRITERIO_DE_PARADA,
-                                          p_mutacao=P_MUTACAO, sigma=SIGMA, alpha=ALPHA,
-                                          limite_inferior=LIMITE_INFERIOR, limite_superior=LIMITE_SUPERIOR,
-                                          realizacao=realizacao, path_graficos=PATH_GRAFICOS)
+    solucao, fitness, fitness_sharing = algoritmo_genetico(n_populacao=N_POPULACAO, q_torneio=Q_TORNEIO,
+                                                           criterio_de_parada=CRITERIO_DE_PARADA,
+                                                           p_mutacao=P_MUTACAO, sigma=SIGMA, alpha=ALPHA,
+                                                           limite_inferior=LIMITE_INFERIOR,
+                                                           limite_superior=LIMITE_SUPERIOR,
+                                                           realizacao=realizacao, path_graficos=PATH_GRAFICOS)
     solucoes.append(solucao)
     fitnesses.append(fitness)
+    fitnesses_sh.append(fitness_sharing)
 
 np.savetxt(fname=PATH_SALVAR_MELHORES_SOLUCOES+'solucoes.txt', X=np.array(solucoes).reshape((len(solucoes), 2)),
            delimiter=',', newline='\n', fmt='%.10f')
@@ -34,4 +37,10 @@ np.savetxt(fname=PATH_SALVAR_MELHORES_SOLUCOES+'fitnesses.txt', X=np.array(fitne
 np.savetxt(fname=PATH_SALVAR_MELHORES_SOLUCOES+'fitnesses-valor-medio.txt', X=np.array([np.mean(fitnesses)]),
            delimiter=',', newline='\n', fmt='%.10f')
 np.savetxt(fname=PATH_SALVAR_MELHORES_SOLUCOES+'fitnesses-desvio-padrao.txt', X=np.array([np.std(fitnesses)]),
+           delimiter=',', newline='\n', fmt='%.10f')
+np.savetxt(fname=PATH_SALVAR_MELHORES_SOLUCOES+'fitnesses-sh.txt', X=np.array(fitnesses_sh).reshape((
+           len(fitnesses_sh), 1)), delimiter=',', newline='\n', fmt='%.10f')
+np.savetxt(fname=PATH_SALVAR_MELHORES_SOLUCOES+'fitnesses-sh-valor-medio.txt', X=np.array([np.mean(fitnesses_sh)]),
+           delimiter=',', newline='\n', fmt='%.10f')
+np.savetxt(fname=PATH_SALVAR_MELHORES_SOLUCOES+'fitnesses-sh-desvio-padrao.txt', X=np.array([np.std(fitnesses_sh)]),
            delimiter=',', newline='\n', fmt='%.10f')
